c49290547ec0b7e7843266ff9a36a50f7cb59708
JuanR66803 <juan.reinosa@correounivalle.edu.co>
2025-04-19 18:14:46 -0500

    implementacion de seccion 1 de conjutivitis

--- src/pages/diseases/Conjuntivitis/model-3d/EyeHealthModel.jsx
+++ src/pages/diseases/Conjuntivitis/model-3d/EyeHealthModel.jsx
@@ -6,9 +6,9 @@ export function EyeHealthModel(props) {
   const { nodes, materials } = useGLTF('/models-3d/Conjuntivitis/health-eye.glb')
   return (
     <group {...props} dispose={null}>
-      <mesh castShadow receiveShadow geometry={nodes.Sclera.geometry} material={materials.Sclera} />
-      <mesh castShadow receiveShadow geometry={nodes.Cornea.geometry} material={materials.Cornea} />
-      <mesh castShadow receiveShadow geometry={nodes.Iris.geometry} material={materials.iris} />
+      <mesh castShadow geometry={nodes.Sclera.geometry} material={materials.Sclera} />
+      <mesh castShadow geometry={nodes.Cornea.geometry} material={materials.Cornea} />
+      <mesh castShadow geometry={nodes.Iris.geometry} material={materials.iris} />
     </group>
   )
 }
