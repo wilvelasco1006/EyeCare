20ef7829e6574172fadb0c3b4b40d8d6468585ca
Athelstan D. Plazas <oliver.plazas@correounivalle.edu.co>
2025-04-25 16:31:24 -0500

    Conflicto resuelto

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -11,7 +11,6 @@ const Header = () => {
   const closeModal = () => setShowModal(false);
 
   return (
-<<<<<<< HEAD
     <>
       <header>
         <nav className="nav">
@@ -31,28 +30,6 @@ const Header = () => {
       {/* Renderiza el modal si está activo */}
       {showModal && <Sign closeModal={closeModal} />}
     </>
-=======
-    <header>
-      <nav className="nav">
-        <img src="/images/nobackground.png" alt="eye" className="logo" />
-        <NavLink to="/" end>
-          Inicio
-        </NavLink>
-        <NavLink to="diseases/content-diseases" end>
-          Enfermedades
-        </NavLink>
-        <NavLink to="quiz" end>
-          Quiz
-        </NavLink>
-        <NavLink to="about-us" end>
-          Sobre nosotros
-        </NavLink>
-        <NavLink to="sign-in" end className="sign-in-link">
-          Sign in
-        </NavLink>
-      </nav>
-    </header>
->>>>>>> odpf
   );
 };
 
