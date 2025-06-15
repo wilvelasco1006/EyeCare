6ad7d4f024e09f9c25b601b8901d11571aa20a9c
Wil <wilmerulcuevelasco@gmail.com>
2025-06-15 11:42:00 -0500

    Sigin.jsx actualizado

--- src/pages/sign-in/Sign.jsx
+++ src/pages/sign-in/Sign.jsx
@@ -3,22 +3,14 @@ import "./Sign.css";
 import { FaTimes } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import useAuthStore from "../../stores/use-auth-store";
-import { useNavigate } from "react-router-dom"; // Asegúrate de usar react-router-dom
+import { useNavigate } from "react-router-dom"; 
 
 const Sign = ({ closeModal }) => {
   const { loginGoogleWithPopup, userLooged } = useAuthStore();
   const navigate = useNavigate();
 
   const createUserInDatabase = useCallback(async (userData) => {
-    console.log("createUserInDatabase llamado con:", userData);
-    const API_BASE = import.meta.env.VITE_API_BASE_URL;
-    console.log("VITE_API_BASE_URL:", API_BASE); // Verifica esto
-    if (!API_BASE) {
-      console.error("¡VITE_API_BASE_URL no está definida!");
-      return; // No intentes el fetch si la URL base no existe
-    }
-    const API_URL = `${API_BASE}users`; // Asegúrate de que la concatenación sea correcta (ej. manejo de '/')
-    console.log("Intentando POST a API_URL:", API_URL); // Verifica la URL completa
+    
     try {
       const response = await fetch(
         `${import.meta.env.VITE_API_BASE_URL}users`, //
@@ -38,33 +30,20 @@ const Sign = ({ closeModal }) => {
   });
 
   const handleLogin = useCallback(() => {
-    console.log("PRIMER LOG: handleLogin ha sido invocado DIRECTAMENTE."); // <--- ¡MUY IMPORTANTE!
+    
     loginGoogleWithPopup()
       .then(async (loggedInUser) => {
-        console.log("Login exitoso, usuario:", loggedInUser); // Este probablemente no se vea
-        closeModal();
-
+        
         if (loggedInUser) {
-          // Verifica que estas propiedades existen y tienen valor
-          console.log("loggedInUser.displayName:", loggedInUser.displayName);
-          console.log("loggedInUser.email:", loggedInUser.email);
-
-          const { displayName, email } = loggedInUser; // Esto debería funcionar
+          console.log("Usuario logueado:", loggedInUser);
+          const { displayName, email } = loggedInUser; 
           const data = { displayName, email };
-          console.log("Preparando para crear usuario en BD con:", data);
+
           await createUserInDatabase(data); // <--- EL FOCO ESTÁ AQUÍ AHORA
         } else {
           // Este bloque ya no debería ejecutarse
           console.log("loggedInUser es null o undefined después del login");
         }
-
-
-        // Crear usuario en la base de datos si el login fue exitoso
-        // if (userLooged) {
-        //   const { displayName, email } = userLooged;
-        //   const data = { displayName, email };
-        //   await createUserInDatabase(data);
-        // }
 
         // Redirige al usuario a la página principal después de iniciar sesión
         navigate("/");
